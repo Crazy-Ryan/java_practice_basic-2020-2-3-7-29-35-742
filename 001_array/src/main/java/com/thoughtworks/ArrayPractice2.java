3c3c7fe59a9282ddb5c20a0b662f4172cad32280
Ryan.Wang <ranwang1995@outlook.com>
2020-02-10 20:08:19 +0800

    [WangRan] fix: correct the coding format of practice 2

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice2.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice2.java
@@ -8,14 +8,14 @@ public class ArrayPractice2 {
     public static int[] exchange() {
         int[] array = new int[]{10, 8, 1, 7, 0, 20, 16, 19};
         int maxNumIndex = 0;
-        for(int index = 0; index < array.length; index++){
-            if(array[index] > array[maxNumIndex]){
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] > array[maxNumIndex]) {
                 maxNumIndex = index;
             }
         }
         int temp;
         temp = array[0];
-        array[0] = array [maxNumIndex];
+        array[0] = array[maxNumIndex];
         array[maxNumIndex] = temp;
         return array;
     }
